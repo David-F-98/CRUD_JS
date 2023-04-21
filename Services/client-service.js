f4c8d2aa1563c3f80308c3b16b27d147ee7a276d
David Nieto <u2903149@unimilitar.edu.co>
2023-04-21 13:35:01 -0500

    v1

--- Services/client-service.js
+++ Services/client-service.js
@@ -1,54 +1,16 @@
-const crearNuevaLinea = (nombre,email) => {
-    const linea = document.createElement("tr");
-    const contenido =  `
-            <td class="td" data-td>${nombre}</td>
-            <td>${email}</td>
-            <td>
-              <ul class="table__button-control">
-                <li>
-                  <a
-                    href="../screens/editar_cliente.html"
-                    class="simple-button simple-button--edit">
-                        Editar
-                    </a>
-                </li>
-                <li>
-                  <button
-                    class="simple-button simple-button--delete"
-                    type="button"
-                  >
-                    Eliminar
-                  </button>
-                </li>
-              </ul>
-            </td>
-          `;
-          linea.innerHTML = contenido;
-          return linea;
+const listaClientes = () => fetch("http://localhost:3000/perfil").then(respuesta => respuesta.json());
+
+const crearCliente = (nombre, email) => {
+    return fetch("http://localhost:3000/perfil", {
+      method: "POST",
+      headers:{
+        "Content-type" : "application/json"
+      },
+      body:JSON.stringify({nombre,email, id:uuid.v4()}),
+    });
+}
+
+export const clientServices = {
+  listaClientes,
+  crearCliente,
 };
-
-
-const table = document.querySelector("[data-table]");
-
-const http = new XMLHttpRequest();
-
-//Abrir http (método,url)
-// CRUD - Métodos  HTTP
-//Create - POST
-//Read - GET
-//Update - PUT/PATCH
-//Delete - DELETE
-http.open("GET","http://localhost:3000/perfil")
-
-http.send();
-
-http.onload = () => {
-    const data = JSON.parse(http.response);
-    console.log(data);
-    data.forEach (perfil =>{
-      const nuevalinea = crearNuevaLinea(perfil.nombre,perfil.email)
-      table.appendChild(nuevalinea);
-    })
-};
-
-console.log(http);
